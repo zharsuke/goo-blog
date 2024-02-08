eb03d4163135344d9188f5365b6a1dc683fcf0f6
zharsuke <alazharisqi@gmail.com>
2024-02-09 03:21:53 +0700

    add index page

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavbarComponents from "../components/NavbarComponents";
+
+export default function Index() {
+    return (
+        <div>
+            <NavbarComponents/>
+            <h1>Testing landing</h1>
+        </div>
+    );
+};
